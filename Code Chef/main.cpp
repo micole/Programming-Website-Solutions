f3593f9c89b678b562288b1d352d95c3f3b4d4ff
micole <micole.3@gmail.com>
2012-06-07 03:24:56 -0400

    Adding code chef folder

---
+++ Code Chef/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: micole
+ *
+ * Created on June 7, 2012, 3:09 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
